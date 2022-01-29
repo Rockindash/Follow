725bbbb7b8dab01d84deaa137081bf8e08cd703b
Amol Kumar <amolkmr10@gmail.com>
2022-01-29 19:10:15 +0530

    Task complete

--- Follow/ViewController.swift
+++ Follow/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
+    var isToggled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func toggleMenu(_ sender: Any) {
+        webView.evaluateJavaScript("document.getElementsByClassName('navbar-toggler svg-svg-20-hamburger svg-image d-lg-none')[0].getAttribute('aria-expanded')", completionHandler: { (html: Any?, error: Error?) in
+            self.isToggled = (html as! String).toBool()
+            self.handleToggle()
+        })
     }
     
+    func handleToggle() {
+        if isToggled {
+            webView.evaluateJavaScript("document.getElementsByClassName('close-button')[0].click()", completionHandler: nil)
+        } else {
+            webView.evaluateJavaScript("document.getElementsByClassName('navbar-toggler svg-svg-20-hamburger svg-image d-lg-none')[0].click()", completionHandler: nil)
+        }
+    }
 }
 
+extension String {
+    func toBool() -> Bool {
+        switch self {
+            case "true":
+                return true
+            default:
+                return false
+        }
+    }
+}
