867c63a160656ba68488075d930ec95e21101463
Amol Kumar <amolkmr10@gmail.com>
2022-01-29 11:38:29 +0530

    WebView added

--- Follow/ViewController.swift
+++ Follow/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleMenu(_ sender: Any) {
+    }
+    
 }
 
